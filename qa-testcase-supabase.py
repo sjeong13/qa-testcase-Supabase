3558ef7ccdb9a9b965291583df8621a89117fe2c
sjeong13 <im.sj@imweb.me>
2025-11-19 16:25:58 +0900

    Create qa-testcase-supabase.py

---
+++ qa-testcase-supabase.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+# 페이지 설정
+st.set_page_config(
+    page_title="테케봇",
+    page_icon="👾",
+    layout="wide"
+)
+
+st.title("👾 테케봇 (QA Test Case Bot)")
+st.write("Hello World!")
